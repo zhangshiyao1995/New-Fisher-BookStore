149ddb23fe0fac1a1d20d2a7e079b45652024c34
zhangshiyao1995 <zhangshiyao1995@users.noreply.github.com>
2018-02-28 21:42:05 -0500

    Lab 5: Angular Continued and display books using api

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,13 @@ namespace Fisher.Bookstore.Api
         {
             services.AddDbContext<BookstoreContext> (opt => opt.UseInMemoryDatabase("Books"));
             services.AddMvc();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +43,7 @@ namespace Fisher.Bookstore.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseCors("CorsPolicy");
 
             app.UseMvc();
         }
